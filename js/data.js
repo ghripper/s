fac69d76337054b7b8cb5e98f34af502db484c15
ghripper <ghripper@163.com>
2020-05-18 19:18:53 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,33 @@
+
+    	
+		new Vue({
+		  el: '#lunbo',
+		  data: {
+            sites: [
+		      // { url: 'img/4.jpg' ,href : 'https://chanshike.cn/?uid=127825' },  //禅试客
+		      { url: 'img/1.jpg' ,href : 'https://www.eimoney.com/diamonds/mobile/share/link?promote_code=3034807' },  //爱钱
+		      { url: 'img/2.jpg' ,href : 'https://xiaoxiao517.com/?u=52894277&referer_code=bc87e230cd' }
+		      // { url: 'img/3.jpg' ,href : 'http://www.baidu.com' }
+		    ]
+        	}
+		})
+
+		new Vue({
+		  el: '#pingtai',
+		  data: {
+		    sites: [
+		      { ico: 'img/imoney.png' ,name: '爱钱' ,href : 'https://im.vsolution.online/landing/guide.html?promote=4468082' },
+		      { ico: 'img/shike.png' ,name: '试客' ,href : 'https://shike.com/i/WQjpHCy' },
+		      { ico: 'img/xiaoyu.jpg' ,name: '小鱼' ,href : 'http://wx.xy599.com/share.php?id=18358348&act_source=invite' },
+		      { ico: 'img/qianmai.jpg' ,name: '钱脉' ,href : 'https://m.qianmaiap.com/?u=1206876' },
+		      { ico: 'img/pp.jpg' ,name: '多多红包' ,href : 'http://sdkw.appjun.com/?r=825212088&d=20200316&en=suryman&t=link&m=1&bt=1' },
+		      { ico: 'img/qianzhuang.jpg' ,name: '钱庄' ,href : 'http://q.zgkeyou.com/download?fromuid=1793432' },
+		      { ico: 'img/chanshike.jpg' ,name: '禅试客' ,href : 'http://csk.limayao.com/?uid=127825' },
+		      { ico: 'img/neihanhongbao.png' ,name: ' 内涵红包' ,href : 'https://app.xiaohanapp.com/Home/SharPage/1366044' },
+		      { ico: 'img/diaoqianyan.png' ,name: '掉钱眼儿' ,href : 'http://invite.chuangqish.cn/?userId=7968018&e=650&source=6' },
+		      { ico: 'img/lanmao.png' ,name: '懒猫' ,href : 'http://cat.cattryapp.com/u/2326804.html' },
+		      { ico: 'img/jubaopeng.png' ,name: '巨宝朋' ,href : 'http://im.jubaopeng9999.com/jbph5/api/sharePage/17119306' },
+		      { ico: 'img/jinrizhuan.png' ,name: '今日赚' ,href : 'https://www.shangbangaso.cn/new-activity/index.html?uid=wrwto4_sign=1lzAgn9h/0Zbiz4tQZWpLbfhwNUipxNuAW8e6IuC4DU=_source=webViewShareNoPc_bundleId=com.posteroliver.NewZipCode' },
+		    ]
+		  }
+		})
